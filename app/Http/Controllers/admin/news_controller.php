ddb778b3275157060e80d422247b64703f4a399c
tahangu <tahangudev@gmail.com>
2024-11-17 12:23:48 -0500

    finish main and page

--- app/Http/Controllers/admin/news_controller.php
+++ app/Http/Controllers/admin/news_controller.php
@@ -29,7 +29,6 @@ class news_controller extends Controller
         $this->view = "admin.module.news.";
         $this->module = "news";
         $this->module_title = __("modules.module_name." . $this->module);
-
         foreach (trans("modules.crud_authorize") as $key => $value) {
             $authorize_name=sprintf("authorize:%s_%s",$key,$this->module);
             $this->middleware($authorize_name)->only($value);
